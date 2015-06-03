b3072e9cc26f05a7e9c0eb9a7ff34a2cab35f50e
danturne <danturne@yahoo.com>
2015-06-03 10:55:08 -1000

    completed exercise

--- elevator.js
+++ elevator.js
@@ -3,4 +3,28 @@
 
 module.exports = {
 
+  goto: function(floorSource,floorTarget) {
+
+    if(typeof floorSource !== "number" ||
+      typeof floorTarget !== "string" ||
+      floorSource < 0 ||
+      floorSource >3 ||
+      parseInt(floorTarget, 10) <0 ||
+      parseInt(floorTarget, 10) >3) {
+
+      return 0;
+    }
+
+    var floorTargetInt = parseInt(floorTarget, 10);
+
+    if(floorSource === floorTargetInt) {
+
+      return 0;
+    }
+
+    var out = floorTargetInt - floorSource;
+
+    return out;
+  }
+
 };
